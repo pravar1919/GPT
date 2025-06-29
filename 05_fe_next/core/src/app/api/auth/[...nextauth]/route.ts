7dbb2aaf2bc241abac57b2cc3f248413c81fadae
Pravar Sharma <pcprakash1919@gmail.com>
2025-06-29 10:01:49 +0530

    updated middleware file

--- 05_fe_next/core/src/app/api/auth/[...nextauth]/route.ts
+++ 05_fe_next/core/src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,9 @@
 // app/api/auth/[...nextauth]/route.ts
 import axios from "@/utils/api"
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
