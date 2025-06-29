aef88937bd56278ee0eef01019fa50c73a9a3bc3
Pravar Sharma <pcprakash1919@gmail.com>
2025-06-29 10:51:08 +0530

    create CRUD for product

---
+++ 05_fe_next/core/src/app/api/products/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from "next/server";
+import ProductSchema from "./schema"
+
+const products = [
+    {
+        id: 1,
+        name: "Milk",
+        price: 3.5
+    },
+    {
+        id: 2,
+        name: "Bread",
+        price: 1.5
+    }
+]
+
+export function GET(request: NextRequest){
+    return NextResponse.json(products, {status: 200})
+}
+
+
+export async function POST(request: NextRequest){
+    let data;
+    try{
+        data = await request.json()
+    }catch (error) {
+        // Handles missing or invalid JSON body
+        return NextResponse.json({ error: 'Invalid or missing JSON body' }, { status: 400 });
+    }
+    
+    const validatedData = ProductSchema.safeParse(data)
+    if (!validatedData.success)
+        return NextResponse.json(validatedData.error.errors, {status: 400});
+    return NextResponse.json(
+        {
+            id: 1,
+            name: validatedData.data.name,
+            price: validatedData.data.price
+        }, 
+        {status: 201})
+}
