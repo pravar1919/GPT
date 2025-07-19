ef2870c01916361db46e4670cb1a5bf5dff907b3
Pravar Sharma <pcprakash1919@gmail.com>
2025-07-19 22:27:25 +0530

    updated code

--- 08_react/src/App.tsx
+++ 08_react/src/App.tsx
@@ -1,16 +1,8 @@
-import { useState } from "react";
-import Button from "./components/Button"
-import Alert from "./components/Alert";
 
 function App() {
-  const [visible, setVisible] = useState(false)
-
   return (
-    <>
-    {visible && <Alert onClose={()=>setVisible(false)}/>}
-    <Button onClick={()=>setVisible(true)}>My Button</Button>
-    </>
-  )
+    <h1>Hello World</h1>
+  );
 }
 
-export default App
+export default App;
