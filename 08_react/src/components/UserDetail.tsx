1d4a643f86e8d5bf3c3ff2bb35be2d83ec0f6615
Pravar Sharma <pcprakash1919@gmail.com>
2025-07-29 12:17:45 +0530

    added location hook

--- 08_react/src/components/UserDetail.tsx
+++ 08_react/src/components/UserDetail.tsx
@@ -1,12 +1,15 @@
-import { useParams, useSearchParams } from "react-router-dom"
+import { useLocation, useParams, useSearchParams } from "react-router-dom"
 
 
 const UserDetail = () => {
     const params = useParams()
     const [searchParams, setSearchParams] = useSearchParams()
+    const location = useLocation()
     console.log(params);
     console.log(searchParams.toString());
     console.log(searchParams.get("filter_id"));
+    console.log(location);
+    
     
   return (
     <div>UserDetail - {params.id}</div>
