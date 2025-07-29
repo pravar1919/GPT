7e8271683811fbc0786276bc7056c63a5c31c379
Pravar Sharma <pcprakash1919@gmail.com>
2025-07-29 17:32:51 +0530

    updated navbar

--- 08_react/src/components/NavBar.tsx
+++ 08_react/src/components/NavBar.tsx
@@ -2,13 +2,25 @@ import { Link } from "react-router-dom"
 
 const NavBar = () => {
   return (
-    <nav>
-        <ul>
-            <li><Link to="/">Navbar</Link></li>
-            <li><Link to="/app">App</ Link></li>
-            <li><Link to="/users">Users</ Link></li>
-        </ul>
-    </nav>
+   <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <Link className="navbar-brand" to="/">Navbar</Link>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarText">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <Link className="nav-link" to="/users">Users</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/app">App</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/login">Login</Link>
+        </li>
+      </ul>
+    </div>
+</nav>
   ) 
 }
 
